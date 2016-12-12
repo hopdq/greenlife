4ea4ad654a9e14d39c40c694c2b7f1fec6241768
DESKTOP-JLLN0RA\hopdq <hopdq1102@gmail.com>
2016-12-12 21:11:48 +0700

    navigator load data example

---
+++ chapifarm.presentation/obj/Release/Package/PackageTmp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="chapifarm.presentation.MvcApplication" Language="C#" %>
---
+++ chapifarm.presentation/obj/Release/Package/PackageTmp/Scripts/home.js
@@ -0,0 +1,13 @@
+﻿$(function () {
+    var bodyModel = ko.observable(new BodyModel());
+    var viewModel = new Layout(bodyModel);
+    ko.applyBindings(viewModel);
+    viewModel.init();
+});
+
+function BodyModel() {
+    var self = this;
+    self.init = function () {
+
+    }
+}
