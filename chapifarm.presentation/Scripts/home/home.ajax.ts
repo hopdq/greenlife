1b6eb33621b21df09f360218d1220496535ea094
DESKTOP-JLLN0RA\hopdq <hopdq1102@gmail.com>
2017-01-09 21:59:59 +0700

    typescript example

---
+++ chapifarm.presentation/Scripts/home/home.ajax.ts
@@ -0,0 +1,10 @@
+﻿class HomeServices {
+    static getProductByCategory(cateId: string): JQueryPromise<Array<ProductDto>> {
+        var dfd = $.Deferred();
+        var url = Utilities.buildApiUrl('api/products/productsByCategory/' + cateId + '/' + DateGetInfoEnum.HomeProductGetNumber);
+        $.get(url, function (data: Array<ProductDto>) {
+            dfd.resolve(data);
+        });
+        return dfd.promise();
+    }
+}
---
+++ chapifarm.presentation/Scripts/shared/shared.entities.ts
@@ -0,0 +1,55 @@
+﻿class CategoryDto {
+    Id: string;
+    Name: string;
+    Url: string;
+    Children: Array<CategoryDto>;
+}
+class BannerDto {
+    Id: string;
+    Text: string;
+    ImgPath: string;
+    Link: string;
+}
+class BannerPageEnum {
+    static Home: string = "HomePage";
+}
+class BannerPosEnum {
+    static Top: string = "Top";
+    static Body: string = "";
+}
+class DateGetInfoEnum {
+    static HomeProductGetNumber: number = 12;
+}
+class ProductDto {
+    public CategoryId: string;
+    public ProviderId: string;
+    public ProductId: string;
+    public Barcode: string;
+    public Name: string;
+    public Summary: string;
+    public Description: string
+    public ImagePath: string
+    public Promotion: string
+    public EndUserPrice: number
+    public Quantity: number
+    public QuantityStatus: string
+    public NameUnsign: string
+    public Keyword: string
+    public MetaTitle: string
+    public MetaDesc: string
+    public View: number
+    public Gift: boolean
+    public New: boolean
+    public QuantityPromotion: number
+    public StartPromotion: Date
+    public CategoryName: string
+    public TinhThanhID: number
+    public customise1: string
+    public customise2: string
+    public customise3: string
+    public DecreasePrice: number
+    public UrlSlug: string
+    public customise4: string
+    public customise5: string
+    public customise6: string
+}
