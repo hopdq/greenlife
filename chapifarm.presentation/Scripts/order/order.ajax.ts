21aa97f5c9e9eb8f9f829b93d4ffbfa5fce21459
FreeStyle-PC\FreeStyle <tuyenbd@ftl.vn>
2017-01-23 19:59:21 +0700

    up lại

--- chapifarm.presentation/Scripts/order/order.ajax.ts
+++ chapifarm.presentation/Scripts/order/order.ajax.ts
@@ -1,25 +1,3 @@
 ﻿
 class OrderServices {
-<<<<<<< .merge_file_a07844
-    static fetchOrderProduct(productIds: Array<string>): JQueryPromise<Array<ProductDto>> {
-        var dfd = $.Deferred();
-        var url = Utilities.buildApiUrl('api/products/productsByIds');
-        $.ajax({
-            type: "POST",
-            url: url,
-            contentType: "application/json",
-            dataType: "json",
-            data: JSON.stringify(productIds),
-            success: function (response) {
-                dfd.resolve(response);
-            },
-            error: function (response) {
-                console.log(response);
-            }
-        });
-        return dfd.promise();
-    }
-=======
-    
->>>>>>> .merge_file_a07372
 }
